d99c239f10c75a6626846343f6d73390d02b7af9
Justin Taylor <jt@justintaylor.org>
2025-04-27 16:06:35 +0100

    Basic accordion works

--- view/frontend/web/js/accordion.js
+++ view/frontend/web/js/accordion.js
@@ -1,34 +1,8 @@
 define(['jquery'], function($) {
 	'use strict';
+	console.log('Accordion module loaded');
 	
-	return function(config) {
-		var defaults = {
-			mediaQuery: '(max-width: 767px)',
-			accordionActive: 'cd-footer-accordion-active',
-			contentSelector: '[data-role="content"]',
-			titleSelector: '[data-role="title"]'
-		};
-		
-		config = $.extend({}, defaults, config);
-		
-		// Handle mobile accordion
-		if (window.matchMedia(config.mediaQuery).matches) {
-			var $titles = $(config.titleSelector);
-			
-			// Initially hide all content on mobile
-			$titles.next(config.contentSelector).hide();
-			
-			// Add click handler
-			$titles.on('click', function() {
-				var $title = $(this);
-				var $content = $title.next(config.contentSelector);
-				
-				// Toggle active class
-				$title.toggleClass(config.accordionActive);
-				
-				// Toggle content with animation
-				$content.slideToggle(300);
-			});
-		}
+	return function(config, element) {
+		console.log('Accordion function executed');
 	};
 });
